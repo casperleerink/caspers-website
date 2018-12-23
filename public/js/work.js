b69b945e8dafdc8e44e566fe1ce28d43b3863f79
casperleerink <casperleerink@gmail.com>
2018-12-23 10:34:24 -0800

    changed work pages del favicon

--- public/js/work.js
+++ public/js/work.js
@@ -1,5 +1,9 @@
-containerDimensions(3, 9, 97, 85, "work");
+containerDimensions(3, 9, 97, 89, "work");
 
+containerDimensions(10, 10, 26, 28, "small1");
+containerDimensions(17, 75, 30, 90, "small2");
+containerDimensions(55, 25, 70, 38, "small3");
+containerDimensions(80, 50, 94, 60, "small4");
 // if (matchMedia('only screen and (max-width: 560px)').matches) {
 //
 // }
